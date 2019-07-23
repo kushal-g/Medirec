ec94b610ba91ea88934aec6d61040baf453d1126
Kucumber <43945468+Kucumber@users.noreply.github.com>
2019-07-24 02:32:49 +0530

    Fixing some animations

---
+++ public/js/allSet.js
@@ -0,0 +1,15 @@
+
+const animation_check=callback=>{
+    $('.fas').animate({ deg: 720,},{duration: 1200,step: function(now) {
+        $(this).css({ transform: 'rotate(' + now + 'deg)' });
+        }
+    });
+}
+
+const animation_content=()=>{
+    $('.content-child').slideDown(1500);
+
+}
+
+animation_check(animation_content());
+
--- public/js/docHome.js
+++ public/js/docHome.js
@@ -1,3 +1,7 @@
 $('.dropdown-toggle').click(function() {
     $(this).next('.dropdown-menu').slideToggle(300);
   });
+
+$('.dropdown-menu').mouseleave(()=>{
+  $('.dropdown-menu').slideUp(500);
+})
