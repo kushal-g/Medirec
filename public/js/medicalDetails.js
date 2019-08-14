34f4708aa02dde5f8682007bbdcb862ddc866b5e
Kucumber <43945468+Kucumber@users.noreply.github.com>
2019-08-15 00:13:03 +0530

    autocomplete genetic disorder

--- public/js/medicalDetails.js
+++ public/js/medicalDetails.js
@@ -44,15 +44,52 @@ socket.emit("sendAccessToken",{},accessToken=>{
             });
             
         });
-
     });
 
     $('#disabilityName').keydown(e=>{
         console.log($(e.target).val());
     });
 
-    $('#geneticalName').keydown(e=>{
-        console.log($(e.target).val());
+    $('#geneticalName').keyup(e=>{
+        const diseaseQuery = $(e.target).val();
+        console.log(diseaseQuery);
+        var settings = {
+            "url": `http://www.healthos.co/api/v1/autocomplete/diseases/${diseaseQuery}`,
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "Authorization": `Bearer ${accessToken}`
+            },
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+            const suggestions_json = response.filter(disease => disease.disease_cat.includes('Genetic diseases') );
+
+            let suggestions=[];
+            for(let i=0;i<suggestions_json.length;i++){
+                suggestions.push({
+                    label:suggestions_json[i].disease_name,
+                    desc:suggestions_json[i].disease_info
+                });
+            }
+
+            console.log(suggestions);
+            $(function(){
+
+                $( "#geneticalName" ).autocomplete({
+                    source: suggestions,
+                    minLength: 3
+                })
+                .autocomplete( "instance" )._renderItem = function( ul, disease ) {
+                    return $( "<li>" )
+                    .append( `<div> ${disease.label} </div> <div class='text-muted'> ${disease.desc.substring(0,70)}...</div><div class='dropdown-divider'></div>` )
+                    .appendTo( ul );
+                };
+            });
+            
+        });
     });
     
 });
